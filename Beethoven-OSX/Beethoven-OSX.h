9b1ed31b65d7100b4bc82dadc4ca780d323fa111
Yifei Teng <teng9@illinois.edu>
2016-01-30 15:26:53 -0600

    OS X Target

---
+++ Beethoven-OSX/Beethoven-OSX.h
@@ -0,0 +1,19 @@
+//
+//  Beethoven-OSX.h
+//  Beethoven-OSX
+//
+//  Created by Yifei Teng on 1/30/16.
+//  Copyright © 2016 Vadym Markov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Beethoven-OSX.
+FOUNDATION_EXPORT double Beethoven_OSXVersionNumber;
+
+//! Project version string for Beethoven-OSX.
+FOUNDATION_EXPORT const unsigned char Beethoven_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Beethoven_OSX/PublicHeader.h>
+
+
